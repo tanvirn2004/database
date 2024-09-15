da7d795a3b5578adeb241a69be3aa4d08d93ce98
tanvirn2004 <TANVIR.NIBIR@metropolia.fi>
2024-09-15 22:06:59 +0300

    k

--- database.md
+++ database.md
@@ -1,1 +1,5 @@
 
+![Screenshot (76).png](..%2F..%2FOneDrive%2FPictures%2FScreenshots%2FScreenshot%20%2876%29.png)
+SELECT country.name AS "country name", airport.name AS "airport name"  
+FROM country JOIN airport ON country.iso_country = airport.iso_country  
+WHERE country.name = 'Iceland';
--- database.py
+++ database.py
@@ -1,20 +1,42 @@
+# import mysql.connector
+#
+# connection = mysql.connector.connect(
+#          host='127.0.0.1',
+#          port= 3306,
+#          database='flight_game',
+#          user='root',
+#          password='2004',
+#          autocommit=True
+#          )
+#
+# def get_airport_by_ident(idao):
+#     sql =f"SELECT name, muncipality FROM airport WHERE ident='{idao}'"
+#     print(sql)
+#     cursor = connection.cursor(deictionary=True)
+#     cursor.execute(sql)
+#     result= cursor.fetchone()
+#     print(result(['name'], result['muncipality']))
+# code = input('Type Icao code:')
+# get_airport_by_ident(co
+# season = ('winter', 'spring', 'summer', 'autumn')
+# month = int(input ('enter the month in number: '))
+# if month in (1, 2 ,12):
+#     print(f'It is {season[0]}')
+# elif month in (3,4,5):
+#     print(f'It is {season[1]}')
+# elif month in (6,7,8):
+#     print(f'It is {season[2]}')
+#
+# else:
+#     print(f'it is {season[3]}')
 import mysql.connector
 
 connection = mysql.connector.connect(
-         host='127.0.0.1',
-         port= 3306,
-         database='people',
-         user='kwargs',
-         password='2004',
-         autocommit=True
+          host='127.0.0.1',
+          port= 3306,
+          database='draft',
+          user='root',
+          password='2004',
+          autocommit=True
          )
 
-def get_airport_by_ident(idao):
-    sql =f"SELECT name, muncipality FROM airport WHERE ident='{idao}'"
-    print(sql)
-    cursor = connection.cursor(deictionary=True)
-    cursor.execute(sql)
-    result= cursor.fetchone()
-    print(result(['name'], result['muncipality']))
-code = input('Type Icao code:')
-get_airport_by_ident(code)
